8da0140825ad647ed7bb68f614c87e0a11bdf591
TOXIC ALIEN <64305844+Alien-Alfa@users.noreply.github.com>
2024-01-30 16:57:04 +0530

    𝞓𝙇𝞘𝞢𝞜-𝞓𝙇𝙁𝞓-𝞛𝘿

--- plugins/dls.js
+++ plugins/dls.js
@@ -6,6 +6,7 @@ const { Readable } = require('stream');
 const ffmpeg = require("../lib/myffmpeg");
 const canvacord = require('canvacord')
 const fetch = require('node-fetch')
+const axios = require("axios");
 
 
 const got = require("got");
@@ -42,107 +43,15 @@ command(
     if (!match) return await message.reply("_Enter Song Name_");
    //await message.reply(`User: ${message.pushName}\nRequest: ${match}`)
 
-    
-let f = await  yts(match)
-let { title, image, author, description } = await f.all[0]
+	  
+let mes = await message.reply(message.jid, { text : _Searching..._ })
+   const res = await axios.get(https://api-viper-x.koyeb.app/api/song?name=${match})
+    let response = await res.data
+	message.client.sendMessage(message.jid, { text : _Downloading : ${response.data.title}_ , edit : mes.key })
+   const songbuff = await (await fetch(${response.data.downloadUrl})).buffer()
+   
+	return await message.client.sendMessage(message.jid , {audio : songbuff ,  mimetype : 'audio/mpeg'} , { quoted : m })
 
-const card = await new canvacord.Spotify()
-.setAuthor(author.name)
-.setAlbum(author.name)
-.setStartTimestamp(1)
-.setEndTimestamp(179000)
-.setBackground('COLOR', '#A30000')
-.setImage(image)
-.setTitle(title)
-
-await await message.client.sendMessage(
-  message.jid,
-  {
-      image: await card.build(),
-      caption: await title.split(',')[0],
-  }
-)
-
-const stream2buffer = async (stream) => {
- const chunks = [];
- return new Promise((resolve, reject) => {
-  stream.on('data', (chunk) => {
-   chunks.push(chunk);
-  });
-  stream.on('end', () => {
-   const buffer = Buffer.concat(chunks);
-
-   resolve(buffer);
-  });
-  stream.on('error', (err) => {
-   reject(err);
-  });
- });
-};
-const downloadMp3 = async (url) => {
- const stream = ytdl(url, {
-  filter: 'audioonly',
-  quality: 'highestaudio'
- });
- const buffer = await stream2buffer(stream);
- return buffer;
-};
-    const buffer = await downloadMp3(q);
-
-
-
-
-
-
-
-
-
-
-
-let COOKIE = `JgFbgOwmjr0TuuGL/A6wKGiRkvCEIiYXXj;88KhdW2GFHKvrnCW/AZPkJoHsDEffqPQ6_;88KhdW2GFHKvrnCW/AZPkJoHsDEffqPQ6_;88KhdW2GFHKvrnCW/AZPkJoHsDEffqPQ6_;VAhsr5JTgy-5zvJLLGBBPXRyKuFBMTFxpzwbCfjg9gExSsLyhU4u9gDfoTtILu_8Zioc6A.;AFvIBn8biMT7vVMETEipYjsCAXtzCMDFmqLSooQH3dmntpOIT_3Jpb4NpDkGRrXIJXsZPwnQBA`    
-const dMp3 = async (Link ) => {
-    	try{
-    	 
-    	 
-
-let im = await getBuffer(image)
-
- 
-const logo = await getBuffer("https://avatars.githubusercontent.com/u/64305844?v=4") 	
-
-            await message.client.sendMessage(message.jid, {
-                audio: buffer,
-                mimetype: 'audio/mpeg',
-                ptt: true,
-                waveform: ["00","99","00","99","00","99","00"],
-                contextInfo: {
-                  externalAdReply: {
-                  title: "YT Downloader",
-                  body: "ᴠᴏɪᴄᴇ : ▮▮▮▮▮▮▯▯▯",
-                  mediaType: 1,
-                  thumbnail: logo,
-                  mediaUrl: 'https://www.instagram.com/alienalfa',
-                  sourceUrl: 'https://www.instagram.com/alienalfa',
-                  }
-              }})
-            await fs.unlinkSync('./media/MOD'+mp3File)
-            await fs.unlinkSync('./media/'+mp3File)
-	
-	
-         
-
-        } catch (err){
-message.reply(err)
-}
-}
-var songId = await mYtId(match)
-if (songId !== null){
-	let Link = 'https://youtu.be/' + songId[1]
-	dMp3(Link)
-	} else {
-		let search = await yts(match)  
-		dMp3(search.all[0].url)
-	}
 }
 );
 command(
