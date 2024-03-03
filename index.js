89595ed5affa1ab85c9a59fea3fac073e09267b1
Your Name <you@example.com>
2024-03-03 19:43:47 +0530

    𝞓𝙇𝞘𝞢𝞜-𝞓𝙇𝙁𝞓-𝞛𝘿

--- index.js
+++ index.js
@@ -226,7 +226,8 @@ async function Tsp() {
                     }
                     try{
                         //await BanStick(msg, conn)
-                        await conn.sendMessage(conn.user.id, {text: msg});
+                        console.log(msg)
+                        //await conn.sendMessage(conn.user.id, {text: msg});
                     } catch(e){ console.log("StickbanERR :"+e)}
                     if (text_msg) {
                         const from = msg.from.endsWith("@g.us") ? `[ ${(await conn.groupMetadata(msg.from)).subject} ] : ${msg.pushName}` : msg.pushName;
