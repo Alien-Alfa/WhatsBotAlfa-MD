7e78a0db0930477c5fe4bfa45c23cf380ee09f01
Your Name <you@example.com>
2024-04-23 06:23:10 -0700

    𝞓𝙇𝞘𝞢𝞜-𝞓𝙇𝙁𝞓-𝞛𝘿

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,82 +1,136 @@
-const {command, fromMe, sleep} = require("../lib")
+const {
+  command,
+  fromMe,
+  sleep
+} = require("../lib")
 
 
 function formatTime(seconds) {
-    if (isNaN(seconds) || seconds < 0) {
+  if (isNaN(seconds) || seconds < 0) {
       return "Invalid input";
-    }
-  
-    const months = Math.floor(seconds / (30 * 24 * 60 * 60));
-    seconds -= months * 30 * 24 * 60 * 60;
-  
-    const days = Math.floor(seconds / (24 * 60 * 60));
-    seconds -= days * 24 * 60 * 60;
-  
-    const hours = Math.floor(seconds / (60 * 60));
-    seconds -= hours * 60 * 60;
-  
-    const minutes = Math.floor(seconds / 60);
-    seconds -= minutes * 60;
-  
-    const timeArray = [];
-  
-    if (months > 0) {
-      timeArray.push(months + (months === 1 ? " month" : " months"));
-    }
-    if (days > 0) {
-      timeArray.push(days + (days === 1 ? " day" : " days"));
-    }
-    if (hours > 0) {
-      timeArray.push(hours + (hours === 1 ? " hour" : " hours"));
-    }
-    if (minutes > 0) {
-      timeArray.push(minutes + (minutes === 1 ? " minute" : " minutes"));
-    }
-    if (seconds > 0) {
-      timeArray.push(seconds + (seconds === 1 ? " second" : " seconds"));
-    }
-  
-    return timeArray.join(", ");
   }
-  
-  
-    
+
+  const months = Math.floor(seconds / (30 * 24 * 60 * 60));
+  seconds -= months * 30 * 24 * 60 * 60;
+
+  const days = Math.floor(seconds / (24 * 60 * 60));
+  seconds -= days * 24 * 60 * 60;
+
+  const hours = Math.floor(seconds / (60 * 60));
+  seconds -= hours * 60 * 60;
+
+  const minutes = Math.floor(seconds / 60);
+  seconds -= minutes * 60;
+
+  const timeArray = [];
+
+  if (months > 0) {
+      timeArray.push(months + (months === 1 ? " month" : " months"));
+  }
+  if (days > 0) {
+      timeArray.push(days + (days === 1 ? " day" : " days"));
+  }
+  if (hours > 0) {
+      timeArray.push(hours + (hours === 1 ? " hour" : " hours"));
+  }
+  if (minutes > 0) {
+      timeArray.push(minutes + (minutes === 1 ? " minute" : " minutes"));
+  }
+  if (seconds > 0) {
+      timeArray.push(seconds + (seconds === 1 ? " second" : " seconds"));
+  }
+
+  return timeArray.join(", ");
+}
+
+
 
 
 command({
-        pattern: "ping",
-        fromMe: true,  
-        desc: "To check ping",
-        type: "user",
-    },
-    async (message, match) => {
-      if(!fromMe(message.user)) return;
-      try{
-   const start = new Date().getTime();
-   await message.client.sendMessage(message.jid, { text: "```Checking Server!```", edit: message.key} );
-        const end = new Date().getTime();
-        setTimeout(async()=>{
-            return await message.client.sendMessage(message.jid, { text: "Ping: "+(end - start) + " ms", edit: message.key} );
-          }, 1000)    
-        } catch (error) {
-          console.error("[Error]:", error);
-        }
-    }
+      pattern: "ping",
+      fromMe: true,
+      desc: "To check ping",
+      type: "user",
+  },
+  async (message, match) => {
+      let me = await fromMe(message.user)
+      if (me) {
+          try {
+              const start = new Date().getTime();
+              await message.client.sendMessage(message.jid, {
+                  text: "```Checking Server!```",
+                  edit: message.key
+              });
+              const end = new Date().getTime();
+              setTimeout(async () => {
+                  return await message.client.sendMessage(message.jid, {
+                      text: "Ping: " + (end - start) + " ms",
+                      edit: message.key
+                  });
+              }, 1000)
+          } catch (error) {
+              console.error("[Error]:", error);
+          }
+      } else if (!me) {
+          try {
+
+              const start = new Date().getTime();
+              let {
+                  key
+              } = await message.reply("```Checking...```");
+              const end = new Date().getTime();
+              setTimeout(async () => {
+                  return await message.client.sendMessage(message.jid, {
+                      text: "Latancy: " + (end - start) + " ms",
+                      edit: key
+                  });
+              }, 1000)
+          } catch (error) {
+              console.error("[Error]:", error);
+          }
+      }
+  }
 );
 command({
-  pattern: "uptime",
-  fromMe: true,  
-  desc: "To check uptime",
-  type: "user",
-},
-async (message, match) => {
-  try{
-   await message.client.sendMessage(message.jid, { text: "```Fetching Uptime...```", edit: message.key} );
-  setTimeout(async()=>{
-      return await message.client.sendMessage(message.jid, { text: "Uptime: "+ await formatTime(process.uptime().toFixed(0)), edit: message.key} );
-    }, 1000)    
-  } catch (error) {
-    console.error("[Error]:", error);
+      pattern: "uptime",
+      fromMe: true,
+      desc: "To check uptime",
+      type: "user",
+  },
+  async (message, match) => {
+      let me = await fromMe(message.user)
+      if (me) {
+          try {
+              await message.client.sendMessage(message.jid, {
+                  text: "```Fetching Uptime...```",
+                  edit: message.key
+              });
+              setTimeout(async () => {
+                  return await message.client.sendMessage(message.jid, {
+                      text: "Uptime: " + await formatTime(process.uptime().toFixed(0)),
+                      edit: message.key
+                  });
+              }, 1000)
+          } catch (error) {
+              console.error("[Error]:", error);
+          }
+
+      } else if (!me) {
+          try {
+              let {
+                  key
+              } = await message.reply("```Fetching Uptime...```");
+              setTimeout(async () => {
+                  return await message.client.sendMessage(message.jid, {
+                      text: "Uptime: " + await formatTime(process.uptime().toFixed(0)),
+                      edit: key
+                  });
+              }, 1000)
+          } catch (error) {
+              console.error("[Error]:", error);
+          }
+      }
+
+
   }
-}
 );
