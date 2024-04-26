b591a7585758b10d9e709bf97cf0863bf731272c
Your Name <you@example.com>
2024-04-26 00:09:09 -0700

    𝞓𝙇𝞘𝞢𝞜-𝞓𝙇𝙁𝞓-𝞛𝘿

--- database/index.js
+++ database/index.js
@@ -1,11 +1,13 @@
 module.exports = {
-    Notes: require("./notes"),
-    Plugins: require("./plugins"),
-    Filters: require("./filters"),
-    Greetings: require("./greetings"),
-    PausedChats:require("./PausedChat"),
-    PDM:require("./pdm"),
-    stickban:require("./stickban"),
-    WarnDB:require('./warn'),
-    NotesDB:require('./notes')
-  };
+  Notes: require("./notes"),
+  Plugins: require("./plugins"),
+  Filters: require("./filters"),
+  Greetings: require("./greetings"),
+  PausedChats:require("./PausedChat"),
+  callAction:require("./callAction"),
+  PDM:require("./pdm"),
+  stickban:require("./stickban"),
+  UserBan:require("./BannedAccount"),
+  WarnDB:require('./warn'),
+  NotesDB:require('./notes')
+};
